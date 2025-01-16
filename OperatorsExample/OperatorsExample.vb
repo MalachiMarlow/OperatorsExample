038b502464347424e336c314aca7bbb1df742371
Tusk <Tuskman045@gmail.com>
2025-01-16 08:52:09 -0700

    Created Console lines.

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -1,6 +1,14 @@
-﻿Module OperatorsExample
+﻿'Malachi Marlow
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/MalachiMarlow/OperatorsExample.git
+
+Module OperatorsExample
 
     Sub Main()
+        Console.Write("5 + 3 = ") 'print the string
+        Console.WriteLine(5 + 3) 'print the sum
 
     End Sub
 
