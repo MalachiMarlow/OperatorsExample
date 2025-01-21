b831a390d47ca4937c25d446a8d79d3890fe727f
Tusk <Tuskman045@gmail.com>
2025-01-21 08:43:18 -0700

    Added comparison operators

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -4,6 +4,8 @@
 'OperatorsExample
 'https://github.com/MalachiMarlow/OperatorsExample.git
 
+Option Compare Text
+
 Module OperatorsExample
 
     Sub Main()
@@ -48,11 +50,38 @@ Module OperatorsExample
         Console.WriteLine(5 ^ 3) 'print exponent
         Console.WriteLine()
 
-        'concatenaiton operator
+        'concatenation operator
         Console.Write("5" & " " & " + " & " 3 " & " = " & " 8 ") 'print the string
         Console.WriteLine()
         Console.WriteLine("wake up samurai..." & vbNewLine & "We got a city to burn...")
         Console.WriteLine(StrDup(10, "Ω"))
+
+        'compare
+        Console.Write("5 > 3 = ") 'print a string
+        Console.WriteLine(5 > 3)
+        Console.WriteLine()
+
+        'compare
+        Console.Write("5 < 3 = ") 'print a string
+        Console.WriteLine(5 < 3)
+        Console.WriteLine()
+
+        'compare
+        Console.Write("does 5 = 3 ? ") 'print a string
+        Console.WriteLine(5 = 3)
+        Console.WriteLine()
+
+        Console.Write("is 5 not equal to 3? ") 'print a string
+        Console.WriteLine(5 <> 3)
+        Console.WriteLine()
+
+        Console.Write("A > B = ") 'print a string
+        Console.WriteLine("A" > "B")
+        Console.WriteLine()
+
+        Console.Write("is " & Chr(34) & "A" & Chr(34) & "the same as " & "a" & " ") 'print a string
+        Console.WriteLine("A" = "a")
+        Console.WriteLine()
 
 
     End Sub
